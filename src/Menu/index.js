688d2addce99a2302d288cb76768dfcaa47fea8a
Thi Vo <thitruongvo@gmail.com>
2018-07-16 23:49:02 +0700

    update style menu bar

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,27 +1,35 @@
 
 import React, { Component } from "react";
 import { Button, Icon } from "@material-ui/core/";
+import { Styl } from "react-dom";
 import logo from '../assets/rocketeer.png'
 import './style.css'
 
 export default class Menu extends Component {
     render() {
         return (
-            <div style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flex: 1, display: 'flex'}}>
-                <Button style={{ width: 90 }}>
-                    About
-            </Button>
-                <Button style={{ width: 90 }}>
-                    Blog
-            </Button>
+            <div style={styles.container}>
+                <div>
+                    <Button style={{ width: 90 }}>About</Button>
+                    <Button style={{ width: 90 }}>Blog</Button>
+                </div>
                 <img src={logo} width={160} />
-                <Button style={{ width: 90 }}>
-                    Projects
-            </Button>
-                <Button style={{ width: 90 }}>
-                    Contact
-            </Button>
+                <div>
+                    <Button style={{ width: 90 }}>Projects</Button>
+                    <Button style={{ width: 90 }}>Contact</Button>
+                </div>
             </div>
         )
+    }
+}
+
+const styles = {
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        display: 'flex',
+        flexWrap: 'wrap',
     }
 }
