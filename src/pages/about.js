1e0e1ba5a20a37d70efb503ce992158610df2294
Thi Vo <thitruongvo@gmail.com>
2018-08-08 23:44:08 +0700

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,12 +3,17 @@ import me from "../assets/me_large.jpg";
 import { introMyself } from "../constants";
 
 const About = () => (
-    <div style={{ flexDirection: 'row', display: 'flex' }}>
+    <div style={{ flexDirection: 'row', display: 'flex'}}>
         <div style={styles.bgImage}>
             <div style={styles.introContainer}>
                 <span style={styles.text}>
-                    Hello<br />
-                    I'm Thi
+                Hi!<br/>
+Thi is a mobile developer in a outsourcing company.<br/>
+At home, he is indie app developer.<br/>
+He also have some experience with backend and design.<br/><br/>
+
+If you want to share something or interested to work with him, feel free to press contact and leave some messages.<br/>
+Peace!<br/>
             </span>
             </div>
         </div>
@@ -26,11 +31,12 @@ const styles = {
         height: '100%', 
         backgroundColor: 'rgba(255,255,255,0.6)',
         minWidth: 320,
+        fontFamily: 'Verdana'
     },
     bgImage: {
         backgroundImage: `url(${me})`, 
-        width: '100%', 
-        height: 600, 
+        width: 720, 
+        height: 498, 
         backgroundSize: 'cover', 
         position: 'relative'
     },
